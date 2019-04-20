7dbd0c024c7054e82a2d41b0f1b4144ffa474670
Matheus Braun <matheeusbl@gmail.com>
2019-04-20 04:16:59 -0300

    App rocketbox
    App do rocketbox

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+	'Unrecognized WebSocket'
+]);
+
+import Routes from './routes';
+
+const src = () => <Routes />;
+
+export default src;
